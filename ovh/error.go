3af24308f3dbeb1dedd0e4e947a350d108eae0cd
cgallot <claire.gallot@corp.ovh.com>
2021-12-07 10:51:31 +0100

    add class and details to APIError
    Signed-off-by: cgallot <claire.gallot@corp.ovh.com>

--- ovh/error.go
+++ ovh/error.go
@@ -4,8 +4,12 @@ import "fmt"
 
 // APIError represents an error that can occurred while calling the API.
 type APIError struct {
+	// Error class
+	Class string
 	// Error message.
 	Message string
+	// Error details
+	Details map[string]string
 	// HTTP code.
 	Code int
 	// ID of the request
