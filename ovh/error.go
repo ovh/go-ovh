328ea2140b52d7b0df3d7a45faeb1c4406168ebd
cgallot <claire.gallot@corp.ovh.com>
2021-12-08 14:43:07 +0100

    add tags for json.Unmarshal
    Signed-off-by: cgallot <claire.gallot@corp.ovh.com>

--- ovh/error.go
+++ ovh/error.go
@@ -5,11 +5,11 @@ import "fmt"
 // APIError represents an error that can occurred while calling the API.
 type APIError struct {
 	// Error class
-	Class string
+	Class string `json:"class,omitempty"`
 	// Error message.
-	Message string
+	Message string `json:"message"`
 	// Error details
-	Details map[string]string
+	Details map[string]string `json:"details,omitempty"`
 	// HTTP code.
 	Code int
 	// ID of the request
