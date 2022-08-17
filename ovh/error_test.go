70282fd6362a9427ef05698578527ff677e05fb6
Romain Beuque <romain.beuque@ovhcloud.com>
2022-08-17 14:26:04 +0000

    feat(errors): refacto display of error messages

--- ovh/error_test.go
+++ ovh/error_test.go
@@ -12,8 +12,20 @@ func TestErrorString(t *testing.T) {
 		Message: "Bad request",
 	}
 
-	expected := `Error 400: "Bad request"`
+	expected := `HTTP Error 400: "Bad request"`
 	got := fmt.Sprintf("%s", err)
+
+	if got != expected {
+		t.Errorf("expected %q, got %q", expected, got)
+	}
+
+	err.Class = "CartAlreadyExists"
+	err.Code = http.StatusConflict
+	err.Message = `the cart id "foobar" already exists`
+	err.QueryID = "EU.ext-99.foobar"
+
+	expected = `HTTP Error 409: CartAlreadyExists: "the cart id \"foobar\" already exists" (X-OVH-Query-Id: EU.ext-99.foobar)`
+	got = fmt.Sprintf("%s", err)
 
 	if got != expected {
 		t.Errorf("expected %q, got %q", expected, got)
